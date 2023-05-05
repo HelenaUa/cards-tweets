c12290494796a69e875081390dbcc39d8ef0ebad
HelenaUa <delena.1408@gmail.com>
2023-05-05 19:40:12 +0300

    5

--- src/components/CardsList/CardsList.styled.jsx
+++ src/components/CardsList/CardsList.styled.jsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledTweetsUl = styled.ul`
-display: flex;
+// display: flex;
+// flex-wrap: wrap;
+// justify-content: center;
+// gap: 20px;
+// padding: 0 16px;
+// min-width: calc(100vw - 91px)
+padding-top: 30px;
+justify-content:center;
+display:flex;
+gap: 30px;
 flex-wrap: wrap;
-justify-content: center;
-gap: 20px;
-padding: 0 16px;
-min-width: calc(100vw - 91px)`
+list-style: none;
+padding-left: 30px;
+padding-right: 30px;`
